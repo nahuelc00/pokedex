c90ac7181cdfaefd95e3e7bdcdfd6fd8dc546ae7
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-05-15 06:34:09 -0300

    update cypress tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,97 +1,121 @@
 /// <reference types="Cypress" />
 
 /* eslint-disable import/extensions */
-/* global cy it context */
+/* global cy it beforeEach context */
 /* eslint no-shadow: */
 
-import { calculateTotalStat, capitalizeFirstLetter, convertHectogramToKilogram } from '../../src/logic.js';
+import { calculateTotalStat, capitalizeFirstLetter, convertHectogramToKilogram } from '../../src/utilities.js';
 
-const URL_API = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
 const URL = '127.0.0.1:8080';
 
 context('Pokedex', () => {
-  it('verify that the pokemon data is shown correctly', () => {
+  beforeEach(() => {
     cy.visit(URL);
-    cy.intercept(URL_API).as('getPokemons');
-    cy.wait('@getPokemons').then((response) => {
-      const pokemons = response.response.body.results;
-      return pokemons;
-    }).then((data) => {
-      const pokemon = data[data.length - 1];
+  });
 
-      cy.intercept(pokemon.url).as(`get${pokemon.name}`);
+  it('ejecute click in pokemon and verify the information', () => {
+    const pokemonData = {
+      name: '',
+      types: [],
+      imgUrl: '',
+      height: '',
+      weight: '',
+      id: 0,
+      abilities: [],
+      stats: [],
+      eggGroups: [],
+      habitat: '',
+    };
 
-      cy.wait(`@get${pokemon.name}`)
-        .then((response) => {
-          const pokemonInfo = response.response.body;
+    cy.intercept('https://pokeapi.co/api/v2/pokemon/6').as('getCharizard');
+    cy.wait('@getCharizard').then((data) => {
+      const infoPokemon = data.response.body;
+      pokemonData.name = infoPokemon.name;
+      pokemonData.imgUrl = infoPokemon.sprites.other.dream_world.front_default;
+      pokemonData.height = infoPokemon.height;
+      pokemonData.weight = infoPokemon.weight;
 
-          return pokemonInfo;
-        })
-        .then((data) => {
-          const { stats } = data;
-          const { abilities } = data;
-          const { types } = data;
-          const { name } = data;
-          const img = data.sprites.other.dream_world.front_default;
-          const { id } = data;
-          const { height } = data;
-          const { weight } = data;
+      infoPokemon.types.forEach((type) => {
+        pokemonData.types.push(type.type.name);
+      });
 
-          return {
-            name, img, types, id, height, weight, abilities, stats,
-          };
-        })
-        .then((data) => {
-          cy.get(`#${data.id}`).find('img').should('have.attr', 'src', data.img);
-          cy.get(`#${data.id}`).find('.card-pokemon__name').should('have.text', capitalizeFirstLetter(data.name));
+      infoPokemon.abilities.forEach((ability) => {
+        pokemonData.abilities.push(ability.ability.name);
+      });
 
-          data.types.forEach((type, index) => {
-            cy.get(`#${data.id}`).find('.card-pokemon__type').eq(index)
-              .should('have.text', capitalizeFirstLetter(type.type.name));
-          });
+      infoPokemon.stats.forEach((stat) => {
+        pokemonData.stats.push({ name: stat.stat.name, base_stat: stat.base_stat });
+      });
+    });
 
-          cy.get(`#${data.id}`).click();
+    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/6').as('getMoreInfoCharizard');
+    cy.wait('@getMoreInfoCharizard').then((data) => {
+      const infoPokemon = data.response.body;
+      pokemonData.habitat = infoPokemon.habitat;
+      infoPokemon.egg_groups.forEach((eggGroup) => {
+        pokemonData.eggGroups.push(eggGroup);
+      });
+    }).then(() => {
+      cy.get('#6').find('.card-pokemon__container-types').children().eq(0)
+        .should('have.text', capitalizeFirstLetter(pokemonData.types[0]));
+      cy.get('#6').find('.card-pokemon__container-types').children().eq(1)
+        .should('have.text', capitalizeFirstLetter(pokemonData.types[1]));
 
-          cy.get('.card-pokemon').each(($card) => {
-            if ($card[0].id === data.id.toString()) {
-              cy.get($card).should('have.not.class', 'd-none');
-            } else {
-              cy.get($card).should('have.class', 'd-none');
-            }
-          });
+      cy.get('#6').click();
+      cy.get('#6').find('.card-pokemon__height').children()
+        .should('have.text', '170 cm');
 
-          cy.get('.img-close-card').should('have.not.class', 'd-none');
+      cy.get('#6').find('.card-pokemon__weight').children()
+        .should('have.text', `${convertHectogramToKilogram(pokemonData.weight)} Kg`);
 
-          data.abilities.forEach((ability, index) => {
-            cy.get(`#${data.id}`).find('.card-pokemon__container-abilities > ul > li').eq(index)
-              .should('have.text', capitalizeFirstLetter(ability.ability.name));
-          });
+      cy.get('#6').find('.card-pokemon__egg-groups').children().eq(0)
+        .should('have.text', capitalizeFirstLetter(pokemonData.eggGroups[0].name));
+      cy.get('#6').find('.card-pokemon__egg-groups').children().eq(1)
+        .should('have.text', capitalizeFirstLetter(pokemonData.eggGroups[1].name));
 
-          data.stats.forEach((stats, index) => {
-            cy.get(`#${data.id}`).find('.card-pokemon__container-stats > ul > li').eq(index)
-              .contains(`${capitalizeFirstLetter(stats.stat.name)}: ${stats.base_stat}`);
-          });
-          cy.get(`#${data.id}`).find('.card-pokemon__container-stats > ul > li').last()
-            .contains(`Total: ${calculateTotalStat(data.stats)}`);
+      cy.get('#6').find('.card-pokemon__abilities').children().eq(0)
+        .should('have.text', capitalizeFirstLetter(pokemonData.abilities[0]));
+      cy.get('#6').find('.card-pokemon__abilities').children().eq(1)
+        .should('have.text', capitalizeFirstLetter(pokemonData.abilities[1]));
 
-          cy.get(`#${data.id}`).find('.card-pokemon__container-height-weight > p').eq(0)
-            .find('span')
-            .should('have.text', `${data.height}0 cm`);
+      cy.get('#6').find('.card-pokemon__stats').children().each(($stat, index) => {
+        if (index < 6) {
+          cy.get('#6').find($stat)
+            .should('have.text', `${capitalizeFirstLetter(pokemonData.stats[index].name)}: ${pokemonData.stats[index].base_stat}`);
+        } else {
+          cy.get('#6').find($stat)
+            .should('have.text', `Total: ${calculateTotalStat(pokemonData.stats)}`);
+        }
+      });
 
-          cy.get(`#${data.id}`).find('.card-pokemon__container-height-weight > p').eq(1)
-            .find('span')
-            .should('have.text', `${convertHectogramToKilogram(data.weight)} Kg`);
+      cy.get('.img-close-card').click();
+      cy.get('#6').should('not.have.class', 'card-expanded');
+    });
+  });
 
-          cy.get(`#${data.id}`).find('.card-pokemon__container-habitat-egg-groups').find('p').contains('Habitat:Grassland');
+  it('verifies next page', () => {
+    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/20').as('getUltimatePokemonInfo');
+    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/40').as('getUltimatePokemonOfNextPage');
 
-          cy.get(`#${data.id}`).find('.card-pokemon__container-habitat-egg-groups').find('ul').contains('Ground');
+    cy.wait('@getUltimatePokemonInfo').then(() => {
+      cy.wait(500);
+      cy.get('.link-navigation-next').click();
+      cy.get('.number-page').should('have.value', 2);
+    });
 
-          cy.get('.img-close-card').click();
+    cy.wait('@getUltimatePokemonOfNextPage').then(() => {
+      cy.wait(2000);
+      cy.get('.card-pokemon').should('have.length', 20);
+    });
+  });
 
-          cy.get('.card-pokemon').each(($card) => {
-            cy.get($card).should('have.not.class', 'd-none');
-          });
-        });
+  it('verifies previous page', () => {
+    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/20').as('getUltimatePokemonInfo');
+    cy.get('.link-navigation-back').click();
+    cy.get('.number-page').should('have.value', 1);
+
+    cy.wait('@getUltimatePokemonInfo').then(() => {
+      cy.get('.card-pokemon').should('have.length', 20);
     });
   });
 });
