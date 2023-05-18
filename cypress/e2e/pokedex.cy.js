73bc3ba5576837d2a04597456ea1c88868ca7ed6
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-05-18 03:29:07 -0300

    update cypress tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,33 +1,56 @@
 /// <reference types="Cypress" />
 
 /* eslint-disable import/extensions */
-/* global cy it beforeEach context */
+/* global cy it describe beforeEach context */
 /* eslint no-shadow: */
 
 import { calculateTotalStat, capitalizeFirstLetter, convertHectogramToKilogram } from '../../src/utilities/utilities.js';
 
 const URL = '127.0.0.1:8080';
 
+function verifyNameOfPokemons(pokemons) {
+  cy.get('.card-pokemon').each(($cardPokemon) => {
+    const pokemonNameOfCard = $cardPokemon.find('.card-pokemon__name').text();
+    const pokemon = pokemons.find((pokemon) => capitalizeFirstLetter(pokemon.name)
+      === pokemonNameOfCard);
+    cy.get($cardPokemon).find('.card-pokemon__name').should('have.text', capitalizeFirstLetter(pokemon.name));
+  });
+}
+
 context('Pokedex', () => {
   beforeEach(() => {
     cy.visit(URL);
   });
 
-  it('ejecute click in pokemon and verify the information', () => {
+  it('Verify pokemons of page', () => {
+    cy.intercept('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20', { fixture: 'list-page-1.json' })
+      .as('getPokemonsOfPage1');
+    cy.get('.number-page:enabled').should('have.value', 1);
+    cy.get('.number-page:disabled').should('have.value', 64);
+
+    cy.wait('@getPokemonsOfPage1').then((data) => {
+      const pokemons = data.response.body.results;
+      verifyNameOfPokemons(pokemons);
+      cy.get('.card-pokemon').should('have.length', 20);
+    });
+  });
+
+  it('Ejecute click in pokemon and verify the information', () => {
     const pokemonData = {
       name: '',
       types: [],
       imgUrl: '',
       height: '',
       weight: '',
-      id: 0,
       abilities: [],
       stats: [],
       eggGroups: [],
       habitat: '',
     };
 
-    cy.intercept('https://pokeapi.co/api/v2/pokemon/6').as('getCharizard');
+    cy.intercept('https://pokeapi.co/api/v2/pokemon/6', { fixture: 'charizard.json' }).as('getCharizard');
+    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/6', { fixture: 'charizard.json' }).as('getInfoOfSpecie');
+
     cy.wait('@getCharizard').then((data) => {
       const infoPokemon = data.response.body;
       pokemonData.name = infoPokemon.name;
@@ -48,8 +71,7 @@ context('Pokedex', () => {
       });
     });
 
-    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/6').as('getMoreInfoCharizard');
-    cy.wait('@getMoreInfoCharizard').then((data) => {
+    cy.wait('@getInfoOfSpecie').then((data) => {
       const infoPokemon = data.response.body;
       pokemonData.habitat = infoPokemon.habitat;
       infoPokemon.egg_groups.forEach((eggGroup) => {
@@ -62,8 +84,9 @@ context('Pokedex', () => {
         .should('have.text', capitalizeFirstLetter(pokemonData.types[1]));
 
       cy.get('#6').click();
+
       cy.get('#6').find('.card-pokemon__height').children()
-        .should('have.text', '170 cm');
+        .should('have.text', `${pokemonData.height}0 cm`);
 
       cy.get('#6').find('.card-pokemon__weight').children()
         .should('have.text', `${convertHectogramToKilogram(pokemonData.weight)} Kg`);
@@ -93,29 +116,56 @@ context('Pokedex', () => {
     });
   });
 
-  it('verifies next page', () => {
-    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/20').as('getUltimatePokemonInfo');
-    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/40').as('getUltimatePokemonOfNextPage');
+  describe('Verify pages', () => {
+    it('Verify next page', () => {
+      cy.intercept('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20', { fixture: 'list-page-2.json' })
+        .as('getPokemonsOfPage2');
 
-    cy.wait('@getUltimatePokemonInfo').then(() => {
-      cy.wait(500);
+      cy.get('.number-page:disabled').should('have.value', 64);
+
       cy.get('.link-navigation-next').click();
-      cy.get('.number-page').should('have.value', 2);
+      cy.get('.number-page:enabled').should('have.value', 2);
+
+      cy.wait('@getPokemonsOfPage2').then((data) => {
+        const pokemons = data.response.body.results;
+        verifyNameOfPokemons(pokemons);
+        cy.get('.card-pokemon').should('have.length', 20);
+      });
     });
 
-    cy.wait('@getUltimatePokemonOfNextPage').then(() => {
-      cy.wait(2000);
-      cy.get('.card-pokemon').should('have.length', 20);
+    it('Verify previous page', () => {
+      cy.intercept('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20', { fixture: 'list-page-1.json' })
+        .as('getPokemonsOfPage1');
+
+      cy.get('.number-page:disabled').should('have.value', 64);
+
+      cy.get('.link-navigation-back').click();
+      cy.get('.number-page:enabled').should('have.value', 1);
+
+      cy.wait('@getPokemonsOfPage1').then((data) => {
+        const pokemons = data.response.body.results;
+        verifyNameOfPokemons(pokemons);
+        cy.get('.card-pokemon').should('have.length', 20);
+      });
     });
-  });
 
-  it('verifies previous page', () => {
-    cy.intercept('https://pokeapi.co/api/v2/pokemon-species/20').as('getUltimatePokemonInfo');
-    cy.get('.link-navigation-back').click();
-    cy.get('.number-page').should('have.value', 1);
+    it('Verify other page', () => {
+      cy.intercept('https://pokeapi.co/api/v2/pokemon/?offset=100&limit=20', { fixture: 'list-page-5.json' })
+        .as('getPokemonsOfPage5');
 
-    cy.wait('@getUltimatePokemonInfo').then(() => {
-      cy.get('.card-pokemon').should('have.length', 20);
+      cy.get('.number-page:enabled').clear();
+      cy.get('.number-page:enabled').type('5');
+      cy.get('.number-page:enabled').should('have.value', 5);
+
+      cy.get('.number-page:disabled').should('have.value', 64);
+
+      cy.get('.link-navigation-next').click();
+
+      cy.wait('@getPokemonsOfPage5').then((data) => {
+        const pokemons = data.response.body.results;
+        verifyNameOfPokemons(pokemons);
+        cy.get('.card-pokemon').should('have.length', 20);
+      });
     });
   });
 });
