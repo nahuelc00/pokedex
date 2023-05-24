ce011587fcc572545300e0d74fbc26a93f5a31f4
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-05-23 23:57:43 -0300

    organize jest tests code

---
+++ src/validations/__tests__/validations.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+/// <reference types="Jest" />
+
+import { checkPokemonHasSpecie } from '../validations.js';
+
+test('Check if the pokemon has specie', () => {
+  const hasSpecie = checkPokemonHasSpecie(1010);
+  expect(hasSpecie).toBe(true);
+});
+
+test('Check if the pokemon has not specie', () => {
+  const hasSpecie = checkPokemonHasSpecie(10001);
+  expect(hasSpecie).toBe(false);
+});
